6df0347f9dfd0e5bae0551bb43b57fad379976f0
Rafael Camargo <contato@rafaelcamargo.com>
2017-10-18 18:33:46 -0200

    Creating book contexts

---
+++ src/scripts/components/open-closed-css-book/open-closed-css-book-component.js
@@ -0,0 +1,10 @@
+(function (){
+  'use strict';
+
+  const openClosedCssBookComponent = {
+    templateUrl: '/components/open-closed-css-book/open-closed-css-book-template.html'
+  };
+
+  angular.module('app').component('openClosedCssBook', openClosedCssBookComponent);
+
+}());
---
+++ src/scripts/components/the-unserious-manifest-book/the-unserious-manifest-component.js
@@ -0,0 +1,10 @@
+(function (){
+  'use strict';
+
+  const theUnseriousManifestBookComponent = {
+    templateUrl: '/components/the-unserious-manifest-book/the-unserious-manifest-book-template.html'
+  };
+
+  angular.module('app').component('theUnseriousManifestBook', theUnseriousManifestBookComponent);
+
+}());
